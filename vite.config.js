5f9b32d6943c717540ea8852f5508654d7500b79
jamesky98 <jamesky98@gmail.com>
2024-05-02 10:35:06 +0800

    0502

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      basicSsl({
+        /** name of certification */
+        name: 'test',
+        /** custom trust domains */
+        domains: ['jamesky98.zapto.org'],
+        /** custom certification directory */
+        certDir: './cert'
+      })
+    ],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),

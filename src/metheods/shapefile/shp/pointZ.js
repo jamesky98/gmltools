d5d7ff8ab88de5b512fec000d02551e37fb385bf
jamesky98 <jamesky98@gmail.com>
2025-08-05 11:52:43 +0800

    fixPointZ

--- src/metheods/shapefile/shp/pointZ.js
+++ src/metheods/shapefile/shp/pointZ.js
@@ -1,3 +1,13 @@
 export default function(record) {
-  return {type: "Point", coordinates: [record.getFloat64(4, true), record.getFloat64(12, true), record.getFloat64(20, true), record.getFloat64(28, true)]};
+  console.log("record", record);
+  if (record.byteLength < 36) {
+    // console.log("record.length", record.byteLength);
+    // get x,y,z
+    return {type: "Point", coordinates: [record.getFloat64(4, true), record.getFloat64(12, true), record.getFloat64(20, true)]};
+  }else{
+    // console.log("record.length", record.byteLength);
+    // get x,y,z,m
+    return {type: "Point", coordinates: [record.getFloat64(4, true), record.getFloat64(12, true), record.getFloat64(20, true), record.getFloat64(28, true)]};
+  }
+  
 };
